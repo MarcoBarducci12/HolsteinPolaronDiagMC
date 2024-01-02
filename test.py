e6a2202b82fac161f59d5ead14b2706b3526a080
Marco Barducci <marcobarducci99@gmail.com>
2024-01-02 12:44:42 +0100

    Tests correctness of methods relative to the remove_phonon update of Polaron class. Use of patch from unittest.mock to mock functions that generate random numbers

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 from os import path
 from polaron import Polaron
 from configparser import ConfigParser
@@ -280,7 +280,7 @@ def test_add_internal(polarons, another_phonon, request):
     {'t_gen': 0.6, 't_rem': 0.8, 'expected_acceptance': 0.68, 'sampled_acceptance' : 0.5},
     {'t_gen': 0.6, 't_rem': 0.8, 'expected_acceptance': 0.68, 'sampled_acceptance' : 0.8},
 ])
-def test_eval_add_internal(polaron, monkeypatch, parameters):
+def test_eval_add_internal(polaron, parameters):
     """This test checks the behaviour of the eval_add_internal method.
      
     It is responsible for evaluating the acceptance probability for the
@@ -294,7 +294,6 @@ def test_eval_add_internal(polaron, monkeypatch, parameters):
 
     Parameters:
         polaron: A Polaron instance with a specific configuration.
-        monkeypatch: A pytest fixture that allows modifying or mocking modules during testing.
         parameters: A list of dictionaries with the patched random values
               and the expected acceptance
     """
@@ -304,22 +303,185 @@ def test_eval_add_internal(polaron, monkeypatch, parameters):
 
     if parameters['expected_acceptance'] == 1.0:
         # Patch the random.uniform function to control its output
-        mock_values = Mock(side_effect=[parameters['t_gen'], 
-                            parameters['t_rem']])
-        monkeypatch.setattr(np.random, 'uniform', mock_values)
-        # Call the method under test
-        polaron.eval_add_internal()
+        with patch('numpy.random.uniform') as mock_uniform:
+            mock_uniform.side_effect = [parameters['t_gen'], 
+                            parameters['t_rem']]
+            # Call the method under test
+            polaron.eval_add_internal()
+
         assert len(polaron.phonon_list) == initial_phonon_list_length + 1
         assert polaron.diagram['order'] == initial_order + 2
 
     elif parameters['expected_acceptance'] < 1.0:
-        mock_values = Mock(side_effect=[parameters['t_gen'], 
-                            parameters['t_rem'], parameters['sampled_acceptance']])
-        monkeypatch.setattr(np.random, 'uniform', mock_values)
-        polaron.eval_add_internal()
+        with patch('numpy.random.uniform') as mock_uniform:
+            mock_uniform.side_effect = [parameters['t_gen'], 
+                            parameters['t_rem'], parameters['sampled_acceptance']]
+            polaron.eval_add_internal()
+
         if parameters['sampled_acceptance'] < parameters['expected_acceptance']:
             assert len(polaron.phonon_list) == initial_phonon_list_length + 1
             assert polaron.diagram['order'] == initial_order + 2
+        else:
+            assert len(polaron.phonon_list) == initial_phonon_list_length
+            assert polaron.diagram['order'] == initial_order
+
+@pytest.fixture
+def polaron_order_four():
+    """This method return a fixed polaron object
+    with two phonons that can be used consistently
+    during the tests
+    """
+    polaron = Polaron(omega=1.0, g=0.5, time=10.0)
+    phonon_1 = np.array([0.2,0.5])
+    polaron.add_internal(phonon_1)
+    phonon_2 = np.array([0.4,0.8])
+    polaron.add_internal(phonon_2)
+    return polaron
+
+def test_weigth_ratio_remove(polaron_order_two, phonon):
+    """This test checks whether the ratio between a diagram
+    with one less phonon and the current one gives the expected value
+    
+    GIVEN: a polaron object of order two with
+      specific parameters and a phonon with specific times
+    WHAT: apply the weight_ratio_remove to evaluate the ratio
+    THEN: the ratio has to be equal to the expected ratio 
+        evaluated through the formula
+    """
+    ratio = polaron_order_two.weigth_ratio_remove(phonon)
+    expected_ratio =  np.exp(10.0 * 1.0 * (0.5 - 0.2)) / ((0.5 * 10.0) ** 2)
+    assert ratio == expected_ratio
+
+@pytest.mark.parametrize("polarons", ["polaron_order_four", 
+                                      "polaron_order_two"])
+def test_proposal_remove_ratio(polarons, phonon, request):
+    """This test checks whether the ratio between the proposal
+    probabilities of the reverse and direct process for the 
+    remove_phonon update gives the expected value
+    
+    GIVEN: a polaron object with specific parameters
+        and a phonon with specific times
+    WHAT: apply the function proposal_remove_ratio to evaluate 
+        the ratio
+    THEN: the ratio has to be equal to the expected ratio 
+        evaluated through the formula
+    """
+    polaron = request.getfixturevalue(polarons)
+    ratio = polaron.proposal_remove_ratio(phonon)
+    if polaron.diagram['order'] == 2:
+        expected_ratio = 2 * 1/(1 - 0.2)
+    else:
+        expected_ratio = (len(polaron.phonon_list))/(1 - 0.2)
+    assert ratio == expected_ratio
+
+@pytest.mark.parametrize("phonon_index", [0,1])
+def test_choose_phonon(polaron_order_four, phonon_index):
+    """This test checks whether the choose_phonon 
+    function retrieves an index from the ones
+    in phonon_list.
+
+    This tests mocks the random sampling of integers 
+    from np.random.randint and checks that during the test
+    the function in called exactly one time as expected
+    
+    GIVEN: a polaron object with specific parameters
+        and a phonon index
+    WHAT: apply the function choose phonon to evaluate 
+        the index
+    THEN: the actual index has to be equal to the 
+        expected index provided as a parameter
+    """
+    with patch('numpy.random.randint') as mock_randint:
+       
+        mock_randint.return_value = phonon_index
+        actual_index = polaron_order_four.choose_phonon()
+
+    mock_randint.assert_called_once_with(len(polaron_order_four.phonon_list))
+    assert actual_index == phonon_index
+
+@pytest.mark.parametrize("parameters", [
+    {'polaron': "polaron_order_four", 'phonon_index': 0},
+    {'polaron': "polaron_order_two", 'phonon_index' :0}])
+def test_remove_internal(parameters, request):
+    """This test checks that the remove_internal method
+    removes a phonon from the phonon_list and updates the order
+    
+    GIVEN: a polaron object with specific parameters
+        and a specific phonon_index 
+    WHAT: apply the function remove_internal to remove a phonon
+        to the phonon list and update the order 
+    THEN: the phonon_list should have no more the phonon indexed
+        by the parameter and the order of the diagram
+        should be decreased by 2
+    """
+    polaron = request.getfixturevalue(parameters['polaron'])
+    #store initial parameters
+    initial_phonon_list_length = len(polaron.phonon_list)
+    initial_diagram_order = polaron.diagram['order']
+    #get expected phonon list after removal
+    expected_phonon_list = [phonon for i,phonon in 
+                            enumerate(polaron.phonon_list) if i!=0]
+    #call to the function, removing first phonon [0.2,0.5]
+    polaron.remove_internal(parameters['phonon_index'])
+
+    #check diagram order and lenght of phonon list is updated correctly
+    assert len(polaron.phonon_list) == initial_phonon_list_length - 1
+    assert polaron.diagram['order'] == initial_diagram_order - 2
+    #check lists are equal phonon by phonon
+    assert all(np.array_equal(actual,expected) for actual,expected in 
+                              zip(polaron.phonon_list, expected_phonon_list))
+
+@pytest.mark.parametrize("parameters", [
+    {'polaron': "polaron_order_four", 'phonon_index' : 0, 'expected_acceptance': 1.0},
+    {'polaron': "polaron_order_two", 'phonon_index' : 0, 'expected_acceptance': 0.64, 'sampled_acceptance' : 0.4},
+    {'polaron': "polaron_order_two", 'phonon_index' : 0, 'expected_acceptance': 0.64, 'sampled_acceptance' : 0.8},
+])
+def test_eval_remove_internal(request, parameters):
+    """This test checks the behaviour of the eval_remove_internal method.
+     
+    It is responsible for evaluating the acceptance probability for the
+    removal of a phonon and eventually removing it from the current 
+    diagram depending on the outcome of Metropolis-Hastings.
+
+    The test involves patching the random number generation to control 
+    the random values used in the evaluation. The method is expected 
+    to remove a phonon from the phonon_list and update the order of the diagram
+    based on the Metropolis-Hastings acceptance criterion.
+
+    Parameters:
+        request: a fixture that can be used to access information
+                from the provided function
+        parameters: A list of dictionaries with the patched random values
+              a specific polaron and the expected acceptance
+    """
+    # Get the initial state
+    polaron=request.getfixturevalue(parameters['polaron'])
+    initial_phonon_list_length = len(polaron.phonon_list)
+    initial_order = polaron.diagram['order']
+
+    if parameters['expected_acceptance'] == 1.0:
+        # Patch the random.uniform function to control its output
+        with patch('numpy.random.randint') as mock_randint:
+            mock_randint.return_value = parameters['phonon_index']  # Replace with the desired index
+            # Call the method under test
+            polaron.eval_remove_internal()
+
+        assert len(polaron.phonon_list) == initial_phonon_list_length - 1
+        assert polaron.diagram['order'] == initial_order - 2
+
+    elif parameters['expected_acceptance'] < 1.0:
+        #setting calls to mock functions for 
+        with patch('numpy.random.randint') as mock_randint, \
+         patch('numpy.random.uniform') as mock_uniform:
+
+            mock_randint.return_value = parameters['phonon_index']
+            mock_uniform.return_value = parameters['sampled_acceptance']
+
+            polaron.eval_remove_internal()
+
+        if parameters['sampled_acceptance'] < parameters['expected_acceptance']:
+            assert len(polaron.phonon_list) == initial_phonon_list_length - 1
+            assert polaron.diagram['order'] == initial_order - 2
         else:
             assert len(polaron.phonon_list) == initial_phonon_list_length
             assert polaron.diagram['order'] == initial_order
